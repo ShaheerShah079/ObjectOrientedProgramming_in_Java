fd4d9773d6dfa9641e118af9244af2c058759f4b
ShaheerShah079 <145196564+ShaheerShah079@users.noreply.github.com>
2024-07-28 02:26:39 +0500

    Add files via upload

--- Java_practise_cde/Library.java
+++ Java_practise_cde/Library.java
@@ -1,168 +1,118 @@
-package com.shaheer;
+
 import java.util.Scanner;
-    class LibraryCLass {
-        Scanner input = new Scanner(System.in);
-        Scanner inputString = new Scanner(System.in);
-        String[] availableBook = new String[90];
-        String[] issueBook = new String[90];
-        String [] temp = new String[90];
-        static int avbook = 7;
-        static int issbook = 0;
-        char ch;
-
-        public LibraryCLass() {
-            availableBook[0] = "c language";
-            availableBook[1] = "c++ language";
-            availableBook[2] = "java language";
-            availableBook[3] = "python language";
-            availableBook[4] = "html language";
-            availableBook[5] = "css language";
-            availableBook[6] = "react language";
-        }
-
-        public void addBook() {
-            System.out.println("Enter the name of the book");
-            String bookName = inputString.nextLine();
-            this.availableBook[avbook] = bookName;
-            avbook++;
-            System.out.println("Book added successfully...........");
-            System.out.println("Press y to add book again.");
-            ch = inputString.nextLine().charAt(0);
-            if (ch == 'y' || ch == 'Y')
-            {
-                addBook();
-            }
-        }
-
-        public void availableBook() {
-            int i = 1;
-            System.out.println("......Available Books are......");
-            for (String element : this.availableBook) {
-                if(element==null){
-                    break;
-                }
-                System.out.print(i + " " + element + ",   ");
-                i++;
-            }
-            System.out.println();
-        }
-
-        public void issueBook() {
-            availableBook();
-            System.out.println("Press no of book you want to Issue.");
-            int var = input.nextInt();
-            this.issueBook[issbook] = this.availableBook[var - 1];
-            issbook++;
-            System.out.println("Book issued successfully...........");
-            System.out.println("Press y to issue book again.");
-            ch = inputString.nextLine().charAt(0);
-            if (ch == 'y' || ch == 'Y')
-            {
-                issueBook();
-            }
-        }
-
-        public void printIssueBook() {
-            System.out.println(".......Issue Books are.......");
-            int i = 1;
-            for (String element : this.issueBook) {
-
-                if(element==null){
-                    break;
-                }
-                System.out.print(i + " " + element + ",  ");
-                i++;
-            }
-            System.out.println();
-        }
-
-        public void removeAvailable() {
-            availableBook();
-            System.out.println("Press no of book you want to remove.");
-            int variable = input.nextInt();
-            this.availableBook[variable -1] = null;
-            System.out.println("Book removed successfully...........");
-            arrayChange(this.availableBook);
-            System.out.println("Press y to remove book again.");
-            ch = inputString.nextLine().charAt(0);
-            if (ch == 'y' || ch == 'Y')
-            {
-                removeAvailable();
-            }
-        }
-        public void returnBook() {
-            printIssueBook();
-            System.out.println("Press no of book you want to return.");
-            int variable = input.nextInt();
-            this.issueBook[(variable) - 1] = null;
-            System.out.println("Book removed successfully...........");
-            arrayChange(this.issueBook);
-            System.out.println("Press y to remove book again.");
-            ch = inputString.nextLine().charAt(0);
-            if (ch == 'y' || ch == 'Y')
-            {
-                returnBook();
-            }
-        }
-
-        public void arrayChange(String [] aray){
-            for(int i=0,j=0;(i<temp.length)&&(j<aray.length);j++){
-                if(aray[j]==null){
-                    continue;
-                }
-                temp[i]=aray[j];
-                i++;
-            }
-            for(int i=0;i<temp.length;i++){
-                aray[i]=temp[i];
-            }
-        }
-
-    }
-
 public class Library {
-        public static void main(String[] args) {
-            Scanner input=new Scanner(System.in);
-            Scanner inputString = new Scanner(System.in);
-            LibraryCLass person = new LibraryCLass();
-            System.out.println("Hello welcome to Shaheer library.Press your choice\n" +
-                    "1.Show Available Books\n" +
-                    "2.Add Book\n" +
-                    "3.Remove Any Available Book\n"+
-                    "4.Issue Book\n" +
-                    "5.Show Issued Book \n"+
-                    "6.Return Book\n");
-            int choice = input.nextInt();
-            while(choice!=5){
-                switch(choice) {
-                    case 1:
-                        person.availableBook();
-                        break;
-                    case 2:
-                        person.addBook();
-                        break;
-                    case 3:
-                        person.removeAvailable();
-                        break;
-                    case 4:
-                        person.issueBook();
-                        break;
-                    case 5:
-                        break;
-                    case 6:
-                        person.returnBook();
-                        break;
-                }
-                System.out.println("Hello welcome to Shaheer library.Press your choice\n" +
-                        "1.Show Available Books\n" +
-                        "2.Add Book\n" +
-                        "3.Remove Any Available Book\n"+
-                        "4.Issue Book\n" +
-                        "5.Show Issued Book \n"+
-                        "6.Return Book\n");
-                choice = input.nextInt();
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        Scanner in1 = new Scanner(System.in);
+        Scanner in2 = new Scanner(System.in);
+        Book obj=new Book();
+        while(true){
+            System.out.println("1.Adding Book\n2.Loan Book\n3.Retrieve Book\n4.Check Copy\n5.Display book information in laibrary\n6.Exit");
+            int choice =in.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+                    System.out.print("Amount of books You added : ");
+                    int amounts =in.nextInt();
+                    for(int i=1;i<=amounts;i++) {
+                        in=new Scanner(System.in);
+                        obj.values++;
+                        System.out.print("Book Title : ");
+                        obj.bTitle[obj.values] = in.nextLine();
+                        System.out.print("Author Name : ");
+                        obj.authors[obj.values] = in.nextLine();
+                        System.out.print("Book Type : ");
+                        obj.bType[obj.values] = in.nextLine();
+                        System.out.print("Published Year : ");
+                        obj.p_year[obj.values] = in.nextInt();
+                        System.out.print("Copies : ");
+                        obj.copy[obj.values] = in.nextInt();
+
+
+
+                        System.out.println("Book "+i+" Added Succesfully");
+                    }
+                    break;
+                case 2:
+                    in=new Scanner(System.in);
+                    System.out.print("Book Title : ");
+                    String titleLoan=in.nextLine();
+                    obj.loanBook(titleLoan);
+
+                    break;
+                case 3:
+                    in=new Scanner(System.in);
+                    System.out.print("Book Title : ");
+                    String titleRetreive=in.nextLine();
+                    obj.retBook(titleRetreive);
+
+                    break;
+                case 4:
+                    in=new Scanner(System.in);
+                    System.out.print("Book Title : ");
+                    String titleCopy=in.nextLine();
+                    obj.checkCopy(titleCopy);
+
+                    break;
+                case 5:
+                    obj.displayInfo();
+                    break;
+                case 6:
+                    System.exit(0);
+
+                    break;
+
             }
-            System.out.println("Thank u for purchasing our books");
-            person.printIssueBook();;
+            System.out.println("\n\n");
+        }
+
+
+    }
+
+}
+class Book {
+    String[] bTitle = new String[100];
+    String[] authors = new String[100];
+    String[] bType = new String[100];
+    int[] p_year = new int[100];
+    int[] copy = new int[100];
+    int values = -1;
+
+    void loanBook(String title) {
+        for (int i = 0; i <=values; i++) {
+            if (bTitle[i].equals(title)) {
+                if (copy[i] != 0) {
+                    copy[i]--;
+                    System.out.println("Thank you Come Again");
+                }
+            }
         }
     }
+
+    void retBook(String title) {
+        for (int i = 0; i <=values; i++) {
+            if (bTitle[i].equals(title)) {
+                copy[i]++;
+                System.out.println("Thank u for Returning");
+            }
+        }
+    }
+
+    void checkCopy(String title) {
+        for (int i = 0; i <=values; i++) {
+            if (bTitle[i].equals(title)) {
+                System.out.println("Number of copies of " + title + " is " + copy[i]);
+            }
+        }
+
+    }
+
+    void displayInfo() {
+        System.out.println("S.  BOOK TITLE     AUTHORS     COPIES");
+        for (int i = 0; i <=values; i++) {
+            System.out.println((i+1)+"   "+bTitle[i]+"   "+authors[i]+"    "+copy[i]);
+        }
+    }
+}
