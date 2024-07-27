88c7ab398ec928ab5b86b2cd9399b9e75cac5051
ShaheerShah079 <145196564+ShaheerShah079@users.noreply.github.com>
2024-07-28 02:02:29 +0500

    Add files via upload

---
+++ NetBean_practise_code/Mywork/src/mywork/CWH_02.java
@@ -0,0 +1,20 @@
+package mywork;
+
+public class CWH_02
+{
+
+    public static void main(String[] args)
+    {
+        byte age = 34;
+        int age2 = 56;
+        long ageDino = 5666666666666666L;
+        short age3 = 337;
+        char ch = 'Á';
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        System.out.println(age);
+        String b = "Imran";
+        System.out.println(b);
+    }
+
+}
