1995dd703b0c6262334e286783e4b51a2994a486
ShaheerShah079 <145196564+ShaheerShah079@users.noreply.github.com>
2024-07-28 03:30:03 +0500

    Add files via upload

---
+++ NetBean_Practise/ExceptionHadling/EXCEPTIONPRAC4.java
@@ -0,0 +1,39 @@
+package com.shaheer;
+class MaxException extends Exception{
+    @Override
+    public String toString() {
+        return "Your 5 entries complete GET LOST!";
+    }
+}
+public class EXCEPTIONPRAC4 {
+    public static void araynum (int [] aray) throws MaxException{
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        int i=0;
+        while(i<6){
+            System.out.println("Enter index");
+            int a=input.nextInt();
+            if(i==5) {
+                throw new MaxException();
+            }
+            try {
+                System.out.println(aray[a]);
+                break;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Error");
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int [] arr = {12,23,43,54,12};
+        try{
+            araynum (arr);
+        }
+        catch(MaxException e){
+            System.out.println(e);
+        }
+    }
+}
+
